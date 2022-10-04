ea5dc8ea408297f9f32e590e75a156ac37d76797
Aleks Cela <alekscela7@gmail.com>
2022-10-04 14:31:12 +0200

    cleaned up some code

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 let button = document.getElementById('button');
 let locationIcon = document.querySelector('.weather-icon');
 
-function getWeatherData( cityname ) {
+function getWeatherData(cityname) {
     var key = 'e0b71890beacc3a4228133815c2b5de3'; //I know i should hide this but its ok , all hail openweathermap free plan 🙏!!!
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityname+ '&appid=' + key)  
-    .then(function(resp) { return resp.json() }) // Convert data to json
-    .then(function(data) {
-      showWeatherData(data);
-      console.log(data);
-    })
-    .catch(function() {
-    });
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityname + '&appid=' + key)
+        .then(function (resp) { return resp.json() }) // Convert data to json
+        .then(function (data) {
+            showWeatherData(data);
+            console.log(data);
+        })
+        .catch(function () {
+        });
 }
 
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
     let cityname = document.getElementById('cityname').value;
-    getWeatherData( cityname );
+    getWeatherData(cityname);
 });
 
 
-function showWeatherData( d ) {
-	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-    const {icon} = d.weather[0];
-	document.getElementById('description').innerHTML =d.weather[0].main;
-	document.getElementById('temp').innerHTML = celcius + '&deg;';
-    document.getElementById('accurate').innerHTML = 'More accurately: '+ d.weather[0].description;
-	document.getElementById('location').innerHTML ='Location: '+ d.name;
+function showWeatherData(d) {
+    var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+    const { icon } = d.weather[0];
+    document.getElementById('description').innerHTML = d.weather[0].main;
+    document.getElementById('temp').innerHTML = celcius + '&deg;';
+    document.getElementById('accurate').innerHTML = 'More accurately: ' + d.weather[0].description;
+    document.getElementById('location').innerHTML = 'Location: ' + d.name;
     locationIcon.innerHTML = `<img src="icons/${icon}.png">`;
 
     if (d.weather[0].main === 'Rain') {
